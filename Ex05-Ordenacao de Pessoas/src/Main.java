144a413b48bae18355aac22a19fc971f8767681b
leonardo <leonardocbizerra@gmail.com>
2025-03-12 10:38:36 -0300

    Correção

--- Ex05-Ordenacao de Pessoas/src/Main.java
+++ Ex05-Ordenacao de Pessoas/src/Main.java
@@ -17,24 +17,27 @@ public class Main {
                 case 1 -> {
                     System.out.println("Digite o nome da pessoa: ");
                     String nome = sc.nextLine();
-                    System.out.println("Digite o altura da pessoa: ");
+                    System.out.println("Digite a altura da pessoa: ");
                     double altura = sc.nextDouble();
-                    System.out.println("Digite o idade da pessoa: ");
+                    System.out.println("Digite a idade da pessoa: ");
                     int idade = sc.nextInt();
                     ordenacaoPessoas.adicionarPessoa(nome,idade,altura);
                     System.out.println("Pessoa adicionada com sucesso!");
                     break;
                 }
                 case 2 -> {
+                    System.out.println("Pessoas ordenadas por Altura: ");
                     System.out.println(ordenacaoPessoas.ordenarPorAltura());
                     break;
                 }
                 case 3 -> {
+                    System.out.println("Pessoas ordenadas por Idade: ");
                     System.out.println(ordenacaoPessoas.ordenarPorIdade());
                     break;
                 }
                 case 4 -> {
                     opcao = 0;
+                    System.out.println("Saindo....");
                     break;
                 }
             }
