7595fd2a38804e67b553eaa179b59a4e343b547a
leonardo <leonardocbizerra@gmail.com>
2025-03-12 10:13:12 -0300

    Ex05

---
+++ Ex05-Ordenacao de Pessoas/src/Main.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        OrdenacaoPessoas ordenacaoPessoas = new OrdenacaoPessoas();
+        Scanner sc = new Scanner(System.in);
+        int opcao = -1;
+
+        do {
+            System.out.println("1-Cadastro de Pessoas");
+            System.out.println("2-Ordenar por Altura");
+            System.out.println("3-Ordenar por Idade");
+            System.out.println("4-Sair");
+            opcao = sc.nextInt();
+            sc.nextLine();
+            switch (opcao) {
+                case 1 -> {
+                    System.out.println("Digite o nome da pessoa: ");
+                    String nome = sc.nextLine();
+                    System.out.println("Digite o altura da pessoa: ");
+                    double altura = sc.nextDouble();
+                    System.out.println("Digite o idade da pessoa: ");
+                    int idade = sc.nextInt();
+                    ordenacaoPessoas.adicionarPessoa(nome,idade,altura);
+                    System.out.println("Pessoa adicionada com sucesso!");
+                    break;
+                }
+                case 2 -> {
+                    System.out.println(ordenacaoPessoas.ordenarPorAltura());
+                    break;
+                }
+                case 3 -> {
+                    System.out.println(ordenacaoPessoas.ordenarPorIdade());
+                    break;
+                }
+                case 4 -> {
+                    opcao = 0;
+                    break;
+                }
+            }
+        }while (opcao != 0);
+    }
+}
---
+++ Ex05-Ordenacao de Pessoas/src/OrdenacaoPessoas.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class OrdenacaoPessoas {
+
+    private List<Pessoa> pessoas;
+
+    public OrdenacaoPessoas() {
+        this.pessoas = new ArrayList<Pessoa>();
+    }
+
+    public void adicionarPessoa(String nome, int idade, double altura){
+        Pessoa pessoa = new Pessoa(nome, idade, altura);
+        pessoas.add(pessoa);
+    }
+
+    public List<Pessoa> ordenarPorIdade() {
+        List<Pessoa> pessoasPorIdade = new ArrayList<>(pessoas);
+        Collections.sort(pessoasPorIdade);
+        return pessoasPorIdade;
+    }
+
+    public List<Pessoa> ordenarPorAltura(){
+        List<Pessoa> peossoasPorAltura = new ArrayList<>(pessoas);
+        Collections.sort(peossoasPorAltura, new ComparatorPorAltura());
+        return peossoasPorAltura;
+    }
+}
